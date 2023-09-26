35dd04c3b80e02f34420f7bb08435df2a76f8883
maheshsutar2410 <124334152+maheshsutar2410@users.noreply.github.com>
2023-09-26 18:03:28 +0530

    Update PenthouseSuiteModified.java

--- module4/SOLID-main/liskov_substitution/good/src/PenthouseSuiteModified.java
+++ module4/SOLID-main/liskov_substitution/good/src/PenthouseSuiteModified.java
@@ -2,5 +2,13 @@
  * Created by mrk on 4/8/14.
  */
 public class PenthouseSuiteModified {
+
+    public PenthouseSuiteModified() {
+        this.numberOfBedrooms = 4;
+    }
+
+    public void setSquareFootage(int sqft) {
+        this.squareFootage = sqft;
+    }
     
 }
